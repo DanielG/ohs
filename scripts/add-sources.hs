aeccfb5c021122a8783f24f3984b1ad87de749db
Daniel Gröber <dxld@darkboxed.org>
2014-12-15 00:33:56 +0100

    Add patched hxt submodule

---
+++ scripts/add-sources.hs
@@ -0,0 +1,48 @@
+#!/usr/bin/env runhaskell
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans.List
+import Control.Monad.Trans.Maybe
+import Control.Monad.IO.Class
+import Data.List
+import System.FilePath
+import System.Directory
+import System.Environment
+import System.Process
+
+max_depth = 3
+
+getDirectoryContents' d = do
+  exists <- doesDirectoryExist d
+  if not exists
+  then return []
+  else
+    map (d</>) . filter (not . flip elem [".", ".."]) <$> getDirectoryContents d
+
+main = do
+  b <- doesFileExist "ohs.cabal"
+  when (not b)
+    $ fail "./ohs.cabal doesn't exist, please run me in the project root."
+
+  root <- canonicalizePath "."
+  fs <- getDirectoryContents' =<< canonicalizePath "submodules"
+
+  mapM_ (addSubmodule 0) =<< filterM doesDirectoryExist fs
+
+addSubmodule depth _ | depth > max_depth = return ()
+addSubmodule depth s = do
+--  putStrLn $ "checking " ++ s
+  fs <- getDirectoryContents' s
+  let b = or $ do
+            f <- fs
+            return $ takeExtension f == ".cabal"
+
+  case b of
+    True -> do
+           putStrLn $ "adding " ++ s
+           callProcess "cabal" ["sandbox", "add-source", s]
+    False -> do
+--      putStrLn $ "no cabal file in: " ++ s
+      addSubmodule (depth+1) `mapM_` fs
--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -2,14 +2,19 @@
 
 cd $(dirname $0)/..
 
+export PATH="$PWD/.cabal-sandbox/bin:$PATH"
+
 git submodule init
-git submodule foreach git fetch
-git submodule update --recursive --checkout --force
+git submodule foreach git reset --hard HEAD
+git submodule foreach git pull origin master
+
+rm -f .cabal-sandbox/add-source-timestamps
 
 [ ! -e cabal.sandbox.config -o ! -e .cabal-sandbox ] && cabal sandbox init
 
-$(dirname $0)/add-sources.sh
+$(dirname $0)/add-sources.hs
 
-[ ! -e .cabal-sandbox/bin/gtk2hsC2hs ] && cabal install gtk2hs-buildtools
-[ ! -e .cabal-sandbox/bin/c2hs ] && cabal install c2hs
-cabal install --enable-tests --only-dependencies --reinstall
+#[ ! -e .cabal-sandbox/bin/gtk2hsC2hs ] && cabal install gtk2hs-buildtools
+#[ ! -e .cabal-sandbox/bin/c2hs ] && cabal install c2hs
+cabal install --enable-tests --only-dependencies --reinstall --force-reinstalls
+cabal configure --enable-tests
